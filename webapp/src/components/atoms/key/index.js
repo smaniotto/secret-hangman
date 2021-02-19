a63ecff1a2651f37bbd6622b0ef734b993dcf986
Felipe <pi.felipesmaniotto@gmail.com>
2021-02-19 14:42:58 -0300

    feat: add used prop to key component

--- webapp/src/components/atoms/key/index.js
+++ webapp/src/components/atoms/key/index.js
@@ -3,10 +3,12 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 import keyBorder from "./key-border.svg";
+import keyUsed from "./key-used.svg";
 
-function Key({ letter = "a" }) {
+function Key({ letter = "a", used = false }) {
   return (
     <div className={styles.container}>
+      {used && <img src={keyUsed} alt="Used key" className={styles.used} />}
       <img src={keyBorder} alt="Key" className={styles.border} />
       <p className={styles.text}>{letter}</p>
     </div>
--- webapp/src/components/atoms/key/index.stories.js
+++ webapp/src/components/atoms/key/index.stories.js
@@ -3,4 +3,6 @@ import { storiesOf } from "@storybook/react";
 
 import Key from "./index";
 
-storiesOf("Key", module).add("Default", () => <Key />);
+storiesOf("Key", module)
+  .add("Default", () => <Key />)
+  .add("Used", () => <Key used={true} />);
