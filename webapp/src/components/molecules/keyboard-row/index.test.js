12f5b198f860e1d2e7c8e5717c83e40fd8af2adf
Felipe <pi.felipesmaniotto@gmail.com>
2021-02-19 15:24:48 -0300

    fix: test keyboard-row instead of key

--- webapp/src/components/molecules/keyboard-row/index.test.js
+++ webapp/src/components/molecules/keyboard-row/index.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import KeyboardRow from ".";
 
 test("Render component successfully", () => {
-  const { getByText } = render(<Key />);
+  const { getByText } = render(<KeyboardRow />);
   const linkElement = getByText(/CKL Create React App Template is ready/i);
   expect(linkElement).toBeInTheDocument();
 });
