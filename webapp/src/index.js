ffb96800b35cdbc2bf85d4b01e5a89f73212dea7
Bernardo Smaniotto <be.smaniotto@gmail.com>
2021-02-22 09:19:10 -0300

    feat: init keplr wallet integration on webapp start

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 
 import Key from "components/atoms/key";
 
+import Wallet from "services/wallet";
+
 import "styles/base.scss";
+
+Wallet.enable();
 
 ReactDOM.render(
   <StrictMode>
