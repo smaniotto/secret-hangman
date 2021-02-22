91a853664d20f13fb5732addb4c21f938f038163
Felipe <pi.felipesmaniotto@gmail.com>
2021-02-22 18:12:18 -0300

    style: change storybook case from default to zero in mistakes component

--- webapp/src/components/atoms/mistakes/index.stories.js
+++ webapp/src/components/atoms/mistakes/index.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from "@storybook/react";
 import Mistakes from ".";
 
 storiesOf("Mistakes", module)
-  .add("Default", () => <Mistakes />)
+  .add("No Mistakes", () => <Mistakes mistakes={0} />)
   .add("1 Mistake", () => <Mistakes mistakes={1} />)
   .add("3 Mistakes", () => <Mistakes mistakes={3} />)
   .add("6 Mistakes", () => <Mistakes mistakes={6} />);
