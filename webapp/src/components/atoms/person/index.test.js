a163ed375cb2ed09d2ed5c49d328d53c7a0ffe7b
Felipe <pi.felipesmaniotto@gmail.com>
2021-02-22 16:56:29 -0300

    feat: add gibbet person component

---
+++ webapp/src/components/atoms/person/index.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Person from ".";
+
+test("Render null as default", () => {
+  const { queryByAltText } = render(<Person />);
+  const image = queryByAltText("Hangman");
+  expect(image).toBeNull();
+});
+
+test("Render component according to number of mistakes", () => {
+  const { queryByAltText } = render(<Person mistakes={3} />);
+  const image3 = queryByAltText("Hangman 3 mistakes");
+  expect(image3).toBeInTheDocument();
+});
