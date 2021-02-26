b75f543aa9102ff0f6fdb15d4261235555633d7c
Felipe <pi.felipesmaniotto@gmail.com>
2021-02-26 20:22:53 -0300

    style: import gibbet structure image to gibbet component

--- webapp/src/components/molecules/gibbet/index.js
+++ webapp/src/components/molecules/gibbet/index.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import Mistakes from "../../atoms/mistakes";
 import Person from "../../atoms/person";
-import Instrument from "../../atoms/instrument";
 
 import styles from "./styles.module.scss";
+
+import gibbetStructure from "./gibbet-structure.svg";
 
 const Gibbet = ({ mistakes = 0 }) => {
   return (
     <div className={styles.container}>
       <div>
-        <Instrument />
+        <img src={gibbetStructure} alt="Gibbet Structure" />
       </div>
       <div className={styles.hangman}>
         <Person mistakes={mistakes} />
