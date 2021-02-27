8c397776b1012c193bea921f5e7fc820da82364c
Felipe <pi.felipesmaniotto@gmail.com>
2021-02-27 18:52:45 -0300

    feat: join main-page and navbar

--- webapp/src/pages/main-page/index.js
+++ webapp/src/pages/main-page/index.js
@@ -3,22 +3,26 @@ import React from "react";
 import Gibbet from "components/molecules/gibbet";
 import WordReveal from "components/molecules/word-reveal";
 import Keyboard from "components/organisms/keyboard";
+import Navbar from "components/organisms/navbar";
 
 import styles from "./styles.module.scss";
 
-const MainPage = ({ mistakes, letters, usedLetters }) => {
+const MainPage = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.upper}>
-        <div className={styles.gibbet}>
-          <Gibbet mistakes={mistakes} />
+    <div>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.upper}>
+          <div className={styles.gibbet}>
+            <Gibbet mistakes={2} />
+          </div>
+          <div className={styles.word_reveal}>
+            <WordReveal letters={["q"]} />
+          </div>
         </div>
-        <div className={styles.word_reveal}>
-          <WordReveal letters={letters} />
+        <div className={styles.lower}>
+          <Keyboard usedLetters={["f", "g"]} />
         </div>
-      </div>
-      <div className={styles.lower}>
-        <Keyboard usedLetters={usedLetters} />
       </div>
     </div>
   );
