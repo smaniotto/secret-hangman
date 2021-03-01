b5aeb0e3bdf3345dbdb0256e25a59fa4e815f590
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-01 16:45:57 -0300

    style: add right border to navbar

--- webapp/src/components/organisms/navbar/index.js
+++ webapp/src/components/organisms/navbar/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import KeplrButton from "../keplr-button";
+import border from "./navbar-border.svg";
 
 import styles from "./styles.module.scss";
 
 const Navbar = () => {
   return (
     <nav className={styles.container}>
+      <img src={border} alt="Navbar Border" className={styles.border} />
       <p className={styles.title}>scrt.hangman</p>
       <KeplrButton />
     </nav>
