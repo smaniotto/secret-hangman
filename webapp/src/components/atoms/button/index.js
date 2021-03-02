1f5d84d6ff60a285e4effb02978bd82b958780cc
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-02 14:18:37 -0300

    style: insert onClick prop to Button component

--- webapp/src/components/atoms/button/index.js
+++ webapp/src/components/atoms/button/index.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import styles from "./styles.module.scss";
 
-const Button = ({ children, className }) => {
-  return <button className={className}>{children}</button>;
+const Button = ({ children, className, onClick }) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
