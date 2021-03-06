136732f76b55b03ab77b59539408ef31aca6c3d9
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-06 19:50:56 -0300

    style: remove duplicate folder

--- webapp/src/components/molecules/loading/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-
-import Loading from ".";
-
-test("Renders successfully", () => {
-  expect(() => render(<Loading />)).not.toThrow();
-});
