4fc0f20826b4da818bdce5f82d78ea6cbadd8041
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-07 15:12:19 -0300

    style: reformat popup to remove redundant code

--- webapp/src/components/atoms/loading/index.js
+++ webapp/src/components/atoms/loading/index.js
@@ -8,10 +8,8 @@ import styles from "./styles.module.scss";
 const Loading = () => {
   return (
     <Popup>
-      <div className={styles.container}>
-        <p className={styles.word}>Loading...</p>
-        <img src={loading} alt="Loading Icon" className={styles.icon} />
-      </div>
+      <p className={styles.word}>Loading...</p>
+      <img src={loading} alt="Loading Icon" className={styles.icon} />
     </Popup>
   );
 };
