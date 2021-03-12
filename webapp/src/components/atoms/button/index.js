886d5edd3062550f2b6164891bc87d5c3c9e1e03
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-12 18:27:08 -0300

    style: add Border to Button component

--- webapp/src/components/atoms/button/index.js
+++ webapp/src/components/atoms/button/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+import Border from "../border";
+
 import styles from "./styles.module.scss";
 
-const Button = ({ children, className, onClick }) => {
+const Button = ({ children, className, onClick, borderWidth, paddingInPixels = [5, 10] }) => {
   return (
-    <button className={`${className} ${styles.button}`} onClick={onClick}>
-      {children}
+    <button
+      className={`${className} ${styles.button}`}
+      style={{ padding: `${paddingInPixels[0]}px ${paddingInPixels[1]}px` }}
+      onClick={onClick}
+    >
+      <Border
+        borderWidth={borderWidth}
+        borderImageOutset={`${paddingInPixels[0] + 1}px ${paddingInPixels[1] + 1}px`}
+      >
+        {children}
+      </Border>
     </button>
   );
 };
