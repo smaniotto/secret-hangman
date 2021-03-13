f3794d5a02ef5120d8905765ea2593baf6ed7500
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-13 11:31:25 -0300

    style: adjust key component style

--- webapp/src/components/atoms/key/index.js
+++ webapp/src/components/atoms/key/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 
+import keyUsed from "./key-used.svg";
+import Button from "../button";
+
 import styles from "./styles.module.scss";
 
-import keyBorder from "./key-border.svg";
-import keyUsed from "./key-used.svg";
-
 const Key = ({ letter, disabled = false, used, onClick }) => {
-  const disabledContainer = `${styles.disabled} ${styles.container}`;
-  const style = !disabled ? styles.container : disabledContainer;
+  const disabledContainer =
+    disabled || used ? `${styles.container} ${styles.disabled}` : styles.container;
 
   return (
-    <div className={style} onClick={onClick}>
+    <Button
+      className={disabledContainer}
+      onClick={onClick}
+      borderWidth={"20px"}
+      paddingInPixels={[0, 7]}
+    >
       {used && <img src={keyUsed} alt="Used key" className={styles.used} />}
-      <img src={keyBorder} alt="Key" className={styles.border} />
+
       <p className={styles.text}>{letter}</p>
-    </div>
+    </Button>
   );
 };
 
--- webapp/src/components/atoms/key/index.stories.js
+++ webapp/src/components/atoms/key/index.stories.js
@@ -4,6 +4,5 @@ import { storiesOf } from "@storybook/react";
 import Key from ".";
 
 storiesOf("Key", module)
-  .add("Default", () => <Key />)
-  .add("Letter A", () => <Key letter="a" />)
-  .add("Letter B Used", () => <Key letter="b" used={true} />);
+  .add("Default", () => <Key letter={"k"} />)
+  .add("Used", () => <Key letter={"k"} used={true} />);
