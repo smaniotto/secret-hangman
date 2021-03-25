396acb5195ad63d229d44b37f5287f4d51d34196
Felipe <pi.felipesmaniotto@gmail.com>
2021-03-25 14:11:05 -0300

    style: move decorators to app scope

--- webapp/src/components/organisms/keplr-connect-button/index.stories.js
+++ webapp/src/components/organisms/keplr-connect-button/index.stories.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import decorators from "@storybook/addon-toolbars";
 
 import KeplrConnectButton from ".";
 
-storiesOf("KeplrConnectButton", module).add("Default", () => (
-  <KeplrConnectButton decorators={decorators} />
-));
+storiesOf("KeplrConnectButton", module).add("Default", () => <KeplrConnectButton />);
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 
 import { WalletProvider } from "context/wallet";
 import MainPage from "pages/main-page";
+import decorators from "@storybook/addon-toolbars";
 
 import "styles/base.scss";
 
 ReactDOM.render(
-  <WalletProvider>
+  <WalletProvider decorators={decorators}>
     <StrictMode>
       <MainPage />
     </StrictMode>
